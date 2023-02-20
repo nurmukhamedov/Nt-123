63e71aebd94f69b919fa3ff4cf6f96344d6d55e4
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-02-20 22:07:28 +0500

    lesson-26

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// alert('Barcha shartlarga rozimisiz');
+
+// console.log('Hello World');
+
+
+
+
+// var x = 6;
+// var first_name = 'Muhammadrasul';
+// var FirstName = 'Ahmadjon'
+
+// console.log(FirstName);
+// console.log(first_name);
+
+// number ma'lumot turi
+
+// var myAge = 22;
+
+// console.log(typeof myAge);
+
+// // string ma'lumot turi
+
+// var jonasAge = '25';
+// var greeting = "Salom men Js dasturchiman";
+// console.log(typeof jonasAge);
+
+// console.log(typeof greeting);
+
+// var isMan = true;
+// var isWoman = false;
+// console.log(typeof isMan);
+// console.log(typeof isWoman);
+
+
+
+// var myJob = null;
+
+// console.log(myJob);
+
+var year = 2023;
+var myBirthYear = 2000;
+var myName = 'Muhammadrasul';
+
+var info = 'Salom mening ismim ' + myName + ' Men ' + (year - myBirthYear) + ' yoshdaman ';
+
+console.log('Salom mening ismim ' + myName + ' Men ' + (year - myBirthYear) + ' yoshdaman ');
