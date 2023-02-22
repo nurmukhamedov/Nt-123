f534eb77154e0c1e5a4aad1c414a89b00bc335ca
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-02-22 22:19:11 +0500

    lesson-27

--- js/index.js
+++ js/index.js
@@ -1,46 +1,95 @@
-// alert('Barcha shartlarga rozimisiz');
+// var age = '22';
+// var hisAge = 21;
 
-// console.log('Hello World');
-
-
-
-
-// var x = 6;
-// var first_name = 'Muhammadrasul';
-// var FirstName = 'Ahmadjon'
-
-// console.log(FirstName);
-// console.log(first_name);
-
-// number ma'lumot turi
-
-// var myAge = 22;
-
-// console.log(typeof myAge);
-
-// // string ma'lumot turi
-
-// var jonasAge = '25';
-// var greeting = "Salom men Js dasturchiman";
-// console.log(typeof jonasAge);
-
-// console.log(typeof greeting);
-
-// var isMan = true;
-// var isWoman = false;
-// console.log(typeof isMan);
-// console.log(typeof isWoman);
+// var isMarried = false;
 
 
+// var userName = prompt('Iltimos ismingizni kiriting');
 
-// var myJob = null;
+// console.log(typeof userName);
 
-// console.log(myJob);
 
-var year = 2023;
-var myBirthYear = 2000;
-var myName = 'Muhammadrasul';
+// const firstName = 'Muhammadrasul';
 
-var info = 'Salom mening ismim ' + myName + ' Men ' + (year - myBirthYear) + ' yoshdaman ';
 
-console.log('Salom mening ismim ' + myName + ' Men ' + (year - myBirthYear) + ' yoshdaman ');
+// console.log(firstName);
+
+// const country = 'Uzbekistan';
+// let population = 30000000;
+// const continent = 'Asia';
+
+// console.log(country);
+// console.log(population);
+// console.log(continent);
+
+// const firstName = 'Muhammadrasul';
+// let age = 23;
+// const myJob = 'Developer';
+
+// console.log(' Mening ismim ' + firstName + ' yoshim ' + '\n' + age + ' va men ' + myJob);
+
+// console.log(`Mening ismim ${firstName} va men tug'ilganman ${2023 - age} yilda kasbim ${myJob} `);
+
+// const num1 = 10;
+
+// const doubledNum = num1 % 3;
+// console.log(doubledNum);
+
+
+// let num = 5;
+// ++num;
+// num++;
+
+// const num1 = 8;
+// const num2 = '8';
+
+// console.log(num1 !== num2);
+
+// const userAge = +prompt('Yoshingiz');
+
+// if (userAge >= 18) {
+//     console.log('Berilsin');
+// } else {
+//     console.log('Berilmasin');
+// }
+
+//Agar user student bo’lsa console ga siz yana ham yaxshi o’qing.
+//Console ga siz yaxshi ishlang.
+
+
+// const userJob = prompt('Sizning kasbingiz');
+
+// if (userJob === 'student') {
+//     console.log('Siz yaxshi oqishingiz kerak');
+// } else if (userJob === 'teacher') {
+//     console.log('Sabr');
+// } else {
+//     console.log('ishlang');
+// }
+
+// const number = +prompt('Raqam kiriting');
+
+// if (number % 2 === 0) {
+//     console.log('Juft son');
+// } else {
+//     console.log('Toq son');
+// }
+
+// prompt orqali 2 ta son olasiz va qaysi son katta ekanligini tekshirasiz agar birinchi son ikkinchisidan katta bo'lsa birinchi son katta, ikkinchi kiritilgan son katta bo'lsa ikkinchisi katta deb chiqsin.
+
+const num1 = +prompt('Birinchi sonni kiriting');
+const num2 = +prompt('Ikkinchi sonni kiriting');
+
+
+if (num1 > num2) {
+    console.log(`Birinchis son katta ${num1 - num2} nisbatda katta`);
+} else if (num1 === num2) {
+    console.log(`Ikkala son ham teng ${num1} va ${num2}`);
+} else {
+    console.log(`Ikkinchi son katta ${num2 - num1} nisbatda katta`);
+}
+
+
+
+
+
