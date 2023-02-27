1d68afb8ff4aadef4b36907e2177e5a58576fa4b
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-02-27 21:51:18 +0500

    lesson-29

---
+++ js/index.js
@@ -0,0 +1,180 @@
+// const johnWeight = +prompt('Jonning vazni');
+// const johnHeight = +prompt('Jonning boyi');
+// const bobWeight = +prompt('Bobning vazni');
+// const bobHeight = +prompt('Bobning boyi');
+
+// const johnBmi = Math.round(johnWeight / johnHeight ** 2);
+// const bobBmi = Math.round(bobWeight / bobHeight ** 2);
+
+
+// if (johnBmi > bobBmi) {
+//     console.log(`John BMI katta ${johnBmi}`);
+// } else if (johnBmi === bobBmi) {
+//     console.log(`Ikkisi ham teng ${johnBmi} va ${bobBmi}`);
+// } else {
+//     console.log(`Bob BMI katta ${bobBmi}`);
+// }
+
+
+// const userAge = Number(prompt('Yoshingizni kiriting'));
+
+// if (userAge <= 18) {
+//     console.log('Siz o`qishingiz kerak');
+// } else if (userAge > 18 && userAge < 60) {
+//     console.log('Siz ishlashingiz kerak');
+// } else {
+//     console.log('Siz pensiyadasiz');
+// }
+
+// let day = 'monday';
+
+// switch (day) {
+//     case 'monday':
+//         console.log('Siz bugun darsga borasiz');
+//         break;
+//     case 'tuesday':
+//         console.log('Siz bugun dam olasiz');
+//          break;
+//     default:
+//      console.log('Bunday kun yo`q');
+//         break;
+// }
+
+// if (day === 'monday') {
+//     console.log('Siz bugun darsga borasiz');
+// } else {
+
+// }
+
+
+
+// const number = Number(prompt('Raqam kiriting'));
+
+// switch (number) {
+//     case 1:
+//         console.log('Dushanba');
+//         break;
+//     case 2:
+//         console.log('Seshanba');
+//         break;
+//     case 3:
+//         console.log('Chorshanba');
+//     default:
+//         console.log('Raqam kiriting');
+//         break;
+// }
+
+// const number = Number(prompt('Raqam kiriting'));
+
+// switch (number % 2) {
+//     case 5:
+//         console.log('Juft');
+//         break;
+//     case 1:
+//         console.log('Toq');
+//         break;
+//     default:
+//         break;
+// }
+
+const age = 22;
+// if (age > 18) {
+//     console.log('Siz mashina hayday olasiz');
+// } else {
+//     console.log('Siz mashina hayday olmaysiz');
+// }
+
+// const user = age > 18 ? 'Siz mashina hayday olasiz' : 'Siz mashina hayday olmaysiz';
+
+// console.log(user);
+
+
+// const population = 30;
+
+// const checkAverage = population >= 34.23 ? 'O`rtachadan yuqori' : 'O`rtachadan past';
+
+// console.log(checkAverage);
+
+// function declaration
+
+// function multiplyNum(num) {
+//     return num * 2;
+// }
+
+// console.log(multiplyNum(5));
+// console.log(multiplyNum(10));
+// console.log(multiplyNum(41));
+
+// function sayGreeting(name) {
+//     return 'Salom ' + name
+// }
+
+
+// console.log(sayGreeting('Sherzod'));
+// console.log(sayGreeting('Ahmadjon'));
+
+
+
+// console.log(addNumber(5));
+// console.log(addNumber(20, 50, 15));
+// console.log(addNumber(30, 50, 4));
+
+// function addNumber(num1, num2 = 0, num3 = 0) {
+//     const sumNumbers = num1 + num2 + num3;
+//     return sumNumbers;
+// }
+
+// // function expression
+
+// let year = 2023;
+
+
+// const myAgeFun = function (birthYear) {
+//     return 2023 - birthYear;
+// }
+
+// // console.log(myAgeFun(2000));
+// // console.log(myAgeFun(2004));
+// // console.log(myAgeFun(2006));
+// // console.log(myAgeFun(2009));
+
+// const myAgeFun2 = (birthYear) => {
+//     return 2023 - birthYear;
+// }
+
+// console.log(myAgeFun2(1999));
+
+
+
+/*
+
+
+Funksiya yozasiz, bu funksiya 1 ta parametr qabul qiladi agar qabul qilingan parametr juft bo'lsa true toq bo'lsa false qaytarsin
+
+*/
+
+
+// function checkNumber(number) {
+//     if (number % 2 === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(checkNumber(15));
+// console.log(checkNumber(17));
+// console.log(checkNumber(18));
+
+// const checkNumbers2 = num => num % 2 === 0 ? true : false;
+
+// console.log(checkNumbers2(20));
+// console.log(checkNumbers2(21));
+
+// const team1 = calcAverage(80, 90, 70)
+//const team2 = calcAverage(150, 80, 50)
+
+
+// (num1 + num2 + num3) / 3 
+
+
