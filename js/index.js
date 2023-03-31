42b8a41f9d9d03eab14fecc5c8821720ad0b4dda
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2023-03-31 07:44:46 +0500

    lesson-42

--- js/index.js
+++ js/index.js
@@ -1,143 +1,115 @@
+// const myBox = document.querySelector('.box');
 
-// const form = document.querySelector('.form');
+// fetch('https://restcountries.com/v3.1/all').then((response) => response.json()).then((data) => {
+//     data.map((country) => {
+//         const countryFlag = document.createElement('img');
+//         countryFlag.src = `${country.flags.png}`;
+//         countryFlag.alt = `${country.flags.alt}`;
 
-// const nameInput = document.querySelector('#name');
-// const price = document.querySelector('#price');
-// const itemId = document.querySelector('#itemId');
-// const itemList = document.querySelector('.itemList');
+//         myBox.appendChild(countryFlag);
+
+//         const countryName = document.createElement('h2');
+//         countryName.textContent = country.name.common;
+
+//         myBox.appendChild(countryName);
+
+//         const populationText = document.createElement('h4');
 
 
-// form.addEventListener('submit', (e) => {
+//         const populationInString = String(country.population);
 
-//     e.preventDefault();
-
-//     if (!nameInput.value && !price.value) {
-//         alert('Malumot kiriting')
-//     }
-
-//     if (itemId.value) {
-//         item = JSON.parse(localStorage.getItem(itemId.value));
-//         item.name = nameInput.value
-//         item.price = price.value
-//     } else {
-//         item = {
-//             name: nameInput.value,
-//             price: price.value
-//         }
-//         itemId.value = `item ${Date.now()}`
-//     }
-
-//     localStorage.setItem(itemId.value, JSON.stringify(item));
-
-//     form.reset();
-//     itemId.value = '';
-//     renderList();
-
-// });
-
-// function renderList() {
-//     itemList.innerHTML = '';
-//     let totalPrice = 0;
-
-//     for (const key in localStorage) {
-//         if (localStorage.hasOwnProperty(key)) {
-
-//             const item = JSON.parse(localStorage.getItem(key));
-//             const li = document.createElement('li');
-
-//             li.innerHTML = `<span>${item.name}</span> <span>$${item.price}</span>`
-//             itemList.appendChild(li);
-
-//             const editBtn = document.createElement('button');
-//             editBtn.textContent = 'Edit';
-//             li.appendChild(editBtn);
-
-//             editBtn.addEventListener('click', () => {
-//                 nameInput.value = item.name;
-//                 price.value = item.price;
-//                 itemId.value = key;
-//             })
-//             const deleteBtn = document.createElement('button');
-//             deleteBtn.textContent = 'Delete';
-//             li.appendChild(deleteBtn);
-
-//             deleteBtn.addEventListener('click', () => {
-//                 localStorage.removeItem(key);
-//                 renderList();
-//             })
-//             totalPrice += Number(item.price);
-
+//         if (populationInString.length === 6) {
+//             populationText.textContent = `${populationInString.slice(0, 3)}k`;
+//             myBox.appendChild(populationText)
+//         } else {
+//             populationText.textContent = populationInString;
+//             myBox.appendChild(populationText)
 //         }
 
+//     })
+// }).catch((error) => console.log(error.message));
+
+// function multiply(num) {
+
+//     return function (factor) {
+//         return num * factor
 //     }
-
-//     const totalItem = document.createElement('h4');
-//     totalItem.textContent = totalPrice;
-
-//     itemList.appendChild(totalItem);
-// }
-
-// renderList();
-
-// const myFragment = document.createDocumentFragment();
-// const box = document.querySelector('.box');
-
-// const myText1 = document.createElement('p');
-// myText1.textContent = 'My text 1';
-
-// const myText2 = document.createElement('p');
-// myText2.textContent = 'My text 2';
-
-// const myText3 = document.createElement('p');
-// myText3.textContent = 'My text 3';
-
-// myFragment.appendChild(myText1);
-// myFragment.appendChild(myText2);
-// myFragment.appendChild(myText3);
-
-// box.appendChild(myFragment);
+// };
+// const double = multiply(5);
+// console.log(double(2));
 
 
-// console.log('Hello World');
-// alert('I am alert')
-// console.log('By World');
-
-
-// let pizza;
-
-// function pizzaOrder() {
-//     console.log('Order Pizza');
-
-//     setTimeout(() => {
-//         pizza = 'My pizza';
-//     }, 2000);
-
-//     console.log('Pizza was ordered');
-// }
-// pizzaOrder();
-
-// setTimeout(() => {
-//     console.log(`Eating ${pizza}`);
-// }, 2500);
-
-const myBox = document.querySelector('.box');
-
-fetch('https://restcountries.com/v3.1/all').then((response) => response.json()).then((data) => {
-    data.map((country) => {
-        const countryFlag = document.createElement('img');
-        countryFlag.src = `${country.flags.png}`;
-        countryFlag.alt = `${country.flags.alt}`;
-
-        myBox.appendChild(countryFlag);
-
-        const countryName = document.createElement('h2');
-        countryName.textContent = country.name.common;
-
-        myBox.appendChild(countryName);
+const imagesUrl = 'https://image.tmdb.org/t/p/w500';
+let url = 'https://api.themoviedb.org/3/discover/movie?api_key=c1ba5d7054ad4225561ffacb783be3c6';
+const searchUrl = 'https://api.themoviedb.org/3/search/movie?api_key=c1ba5d7054ad4225561ffacb783be3c6&query=';
 
 
 
-        console.log(country);
 
-    })
-}).catch((error) => console.log(error.message));
+const fragment = document.createDocumentFragment();
+const moviesList = document.querySelector('.movies-list');
+
+const loaderContainer = document.querySelector('.loader_container')
+const loading = document.createElement('div');
+loading.classList.add('loader');
+loaderContainer.appendChild(loading);
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+
+async function fetchData() {
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        url = `${searchUrl}${searchTerm}`
+    } else {
+        url = 'https://api.themoviedb.org/3/discover/movie?api_key=c1ba5d7054ad4225561ffacb783be3c6'
+    }
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+
+        moviesList.innerHTML = '';
+
+        data.results.map((movie) => {
+
+            const image = document.createElement('img');
+            image.src = `${imagesUrl}${movie.backdrop_path}`;
+            image.alt = movie.title;
+
+            const title = document.createElement('h2');
+            title.textContent = movie.title;
+
+            fragment.appendChild(image);
+            fragment.appendChild(title);
+
+            moviesList.appendChild(fragment);
+
+            console.log(movie);
+
+        })
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+
+fetchData();
+
+btn.addEventListener('click', () => {
+    fetchData();
+})
+search.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        setTimeout(() => {
+            fetchData();
+        }, 2000);
+    }
+})
